e0dd294156aafa3650671f5542d80ae2f264ac60
RENEE\Renee <reneesaulnier@outlook.com>
2023-07-21 23:16:52 -0600

    Updating UI.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './components/Experience';
 
 
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
+
 const startGame = () => {
   root.render(
     <React.StrictMode>
